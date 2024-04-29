f27bbdd1ded452dbadc8801e04a290bd1bd2d848
逗逗飞 <>
2024-04-29 09:59:42 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
+<!--
+ * @Author: 逗逗飞 
+ * @Date: 2024-04-29 09:54:01
+ * @LastEditors: 逗逗飞 
+ * @LastEditTime: 2024-04-29 09:59:21
+ * @FilePath: /cherry-pick-module/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
